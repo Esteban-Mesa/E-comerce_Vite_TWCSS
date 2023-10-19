c5b74db97d9552238e88c6fee0ecfd5af27bd578
Esteban Mesa <esteban2002mesa@gmail.com>
2023-10-19 16:41:37 -0500

    Feat: integrated the total sum of products to the CheckoutSideMenu component, by means of the 'totalPrice' function created in 'utils'.

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { ShoppingCartContex } from "../../Context";
 import OrderCard from "../OrderCard";
+import { totalPrice } from "../../utils";
 
 function CheckoutSideMenu() {
   const { isCheckoutSideMenuOpen, closeCheckoutSideMenu, cartProducts, setCartProducts, setCount } =
@@ -11,20 +12,21 @@ function CheckoutSideMenu() {
     const filteredProducts = cartProducts.filter((product) => product.id != id);
     setCartProducts(filteredProducts);
     setCount(cartProducts.length - 1);
+    console.log(cartProducts);
   };
 
   return (
     <aside
       className={`${
         isCheckoutSideMenuOpen ? null : "hidden"
-      } bg-white flex flex-col fixed right-0 border border-black rounded-lg w-[360px] h-[calc(100vh-68px)] top-[68px] `}>
+      } bg-white flex flex-col justify-between fixed right-0 border border-black rounded-lg w-[360px] h-[calc(100vh-68px)] top-[68px] `}>
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">My order</h2>
         <button type="button" onClick={() => closeCheckoutSideMenu()}>
           <XMarkIcon className="h-6 cursor-pointer" />
         </button>
       </div>
-      <div className="overflow-y-auto">
+      <div className="overflow-y-auto h-full">
         {cartProducts.map((product) => (
           <OrderCard
             key={product.id}
@@ -35,6 +37,12 @@ function CheckoutSideMenu() {
             handleDelete={handleDelete}
           />
         ))}
+      </div>
+      <div className="px-6">
+        <p className="flex justify-between items-center">
+          <span className="font-light">Total:</span>
+          <span className="font-medium text-2xl">${`${totalPrice(cartProducts)}`}</span>
+        </p>
       </div>
     </aside>
   );
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+/**
+ * This function calculates total price of a new order
+ * @param {Array} arryProducts cartProduct: Array of Objects
+ * @returns {Number} Total price 
+ */
+export const totalPrice = (arryProducts) => {
+  return arryProducts.reduce((sum, produtc) => sum + produtc.price, 0);
+};
